9418d02b144b324dece68a7ec2fa19eed1202ffb
Phantominum <robinjerome23@gmail.com>
2022-01-27 18:14:07 +0800

    updated main.py and tested music21 logic, added sample_codes

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ from dist2.MyGrammerParser import MyGrammerParser
 from dist2.MyGrammerVisitor import MyGrammerVisitor
 from dist2.MyGrammerListener import MyGrammerListener
 from MusicNodes import *
+from music21 import *
 
 
 class MyVisitor(MyGrammerVisitor):
     variables = {}
+    music_stream = stream.Stream()
+   
 
     # def visitExpr_note(self, ctx: MyGrammerParser.Expr_chordContext):
     #     val = ctx.INTEGER()
@@ -48,7 +51,7 @@ class MyVisitor(MyGrammerVisitor):
     #               str(note_value_line) + "," + str(note_value_column) + ")")
     #     return self.visitChildren(ctx)
 
-    def visitDeclaredNotes(self, ctx):
+    def visitDeclaredNotes(self, ctx: MyGrammerParser.Declare_noteContext):
         # DECLARED NOTES
         for note in ctx:
             # Get all the DeclareNoteNodes
@@ -57,7 +60,9 @@ class MyVisitor(MyGrammerVisitor):
             if temp.identifier.getText() not in self.variables:
                 note_value = temp.note.note_value.getText()
                 pitch = temp.note.pitch.getText()
+
                 num = temp.note.num.getText()
+
                 dotted = temp.note.dotted
                 self.variables[temp.identifier.getText()] = (note_value, pitch,
                                                              num, dotted)
@@ -67,22 +72,58 @@ class MyVisitor(MyGrammerVisitor):
                 raise Exception(
                     "Reassignment is not allowed. Use a different identifier",
                     line, col)
+            
+           
+        # print("variables", self.variables)
+        return self.variables
 
-        print("variables", self.variables)
-
-    def visitDeclaredChords(self, ctx):
+    def visitDeclaredChords(self, ctx: MyGrammerParser.Declare_chordContext):
         print(ctx)
 
     def visit(self, node):
         # BPM Value
+        notes = []
         print("bpm (" + str(node.bpm) + ")")
 
         # DECLARED NOTES
-        self.visitDeclaredNotes(node.notes)  # Returns NoteExpression Objects
+        note_vars = self.visitDeclaredNotes(node.notes)  # Returns NoteExpression Objects
+        
+        for x in note_vars:
+            num  = note_vars[x][2]
+            pitch = note_vars[x][1]
+            val = note_vars[x][0]
+
+            # create notes
+            v= note.Note(pitch+num)
+            #update note duration
+            if val == "eighth":
+                d = duration.Duration(type="eigth")
+                v.quarterLength = d.quarterLength
+            if val == "sixteenth":
+                v.quarterLength = 0.25
+            if val == "full":
+                d = duration.Duration(type="whole")
+                v.quarterLength = d.quarterLength
+            if val == "double":
+                d = duration.Duration(type="double")
+                v.quarterLength = d.quarterLength
+            if val == "half":
+                d = duration.Duration(type="half")
+                v.quarterLength = d.quarterLength
+            notes.append(v)
+                
+
+
         # DECLARE CHORDS
         self.visitDeclaredChords(node.chords)
         # for chord in declared_chords:
         # TO DO: Chord
+
+
+        #add to stream
+        for x in notes:
+            self.music_stream.append(x)
+        self.music_stream.write('midi', fp='test.midi')
         return "MIDI FILE"
 
 
@@ -94,7 +135,11 @@ if __name__ == "__main__":
         data = InputStream(f.read())
 
     # lexer
-    lexer = MyGrammerLexer(data)
+    # try:
+        lexer = MyGrammerLexer(data)
+    # except Exception as err:
+    #     print(err)
+
     stream = CommonTokenStream(lexer)
     # stream.fill()
     # for token in stream.tokens:
@@ -102,19 +147,25 @@ if __name__ == "__main__":
     #         print(token.text, lexer.symbolicNames[token.type])
 
     # parser
+    
     parser = MyGrammerParser(stream)
     tree = parser.prog()
-    # print(parser.getCurrentToken())
+    if parser._syntaxErrors==0:
+        # print(parser.getCurrentToken())
 
-    # print(tree.toStringTree())
-    # evaluator
-    # print(type(tree), tree)
-    try:
-        ast = MyGrammerVisitor().visitProg(tree)
-        output = MyVisitor().visit(ast)
-    except Exception as err:
-        print("Caught Error: %s in line:%d col:%d" %
-              (err.args[0], err.args[1], err.args[2]))
+        # print(tree.toStringTree())
+        # evaluator
+        # print(type(tree), tree)
+        try:
+            
+            ast = MyGrammerVisitor().visitProg(tree)
+            output = MyVisitor().visit(ast)
+        
+        except Exception as err:
+            print("Caught Error: %s in line:%d col:%d" %
+                (err.args[0], err.args[1], err.args[2]))
+    else:
+        print("Failed to parse")
 
     # printer = MyGrammerPrintListener()
     # walker = ParseTreeWalker()
